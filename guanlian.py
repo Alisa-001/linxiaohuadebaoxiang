6e4fabb3b84a2787c69418e020bc607a6f3d3bfa
优秀秀秀 <19300602715@163.com>
2022-03-29 17:34:52 +0800

    第二次提交

---
+++ guanlian.py
@@ -0,0 +1,12 @@
+import request
+
+# 创建一个请求
+url = 'http://t.api.nowyingshi.com/honor/user/login'
+params = {
+    'platform':'wwa1'
+    'plattype':'aphone'
+    'version':'2.6.0'
+    'login_type':1
+    'phone_number':'16800000001'
+    'password':'8888'
+}
